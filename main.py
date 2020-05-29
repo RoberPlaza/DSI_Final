9dab4b4887f7491e2af902fa83c1e4b141a58966
Rober P. Romero <Roberto.Plaza@alu.uclm.es>
2020-05-29 15:27:53 +0200

    Fixed the fucking model

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from keras.models import Sequential
 
 ### Función distancia. La defino aquí por si la quiero cambiar más adelante.
 distance_f  = lambda pair :len( pair[ 1 ].stepsTaken )
+# distance_f  = lambda pair :pair[ 1 ].distance
 
 ### Algunas constantes globales
 target_ccaa = "CM"                                                              # Nos centramos en Castilla-La Mancha
@@ -46,7 +47,7 @@ similar_ca  = [ pair[ 0 ] for pair in sorted( distancias.items(), key = distance
 ### Preinicializamos los arrays de train y test
 train_x     = np.ones( ( train_ccaa, train_days, len( target_cols ) ) )         # Datos de entreno X
 train_y     = np.ones( ( train_ccaa, train_days, len( target_cols ) ) )         # Datos de entreno Y
-clm_x       = clm_data.iloc[ : 30,  : ][ target_cols ].to_numpy()
+clm_x       = clm_data.iloc[ : train_days,  : ][ target_cols ].to_numpy()
 
 ### Creamos los conjuntos de train y test
 for i in range( train_ccaa ):
@@ -60,48 +61,72 @@ train_y     = np.reshape( train_y, ( train_y.shape[ 0 ], train_y.shape[ 2 ], tra
 clm_x       = np.reshape( clm_x, ( 1, 1, clm_x.shape[ 0 ] ) )
 
 ### Creamos el modelo de predicción con keras
-model       = Sequential()
+model       = Sequential( )
 model.add( LSTM( train_days, return_sequences = True, input_shape = ( len( target_cols ), train_days ) ) )
 
-model.add( LSTM( train_days * 2, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 30, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 7, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 30, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 7, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 2, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 7, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
-
-model.add( LSTM( train_days * 2, return_sequences = True ) )
-model.add( Dropout( 0.20 ) )
+# =============================================================================
+# model.add( LSTM( train_days * 2, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 30, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 7, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 30, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 7, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 2, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 7, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# 
+# model.add( LSTM( train_days * 2, return_sequences = True ) )
+# model.add( Dropout( 0.50 ) )
+# =============================================================================
 
 model.add( LSTM( train_days * 30, return_sequences = True ) )
 model.add( Dense( 30 ) )
 
 ### Entrenamos el modelo
 model.compile( loss='mean_squared_error', optimizer='adam' )
-model.fit( train_x, train_y, epochs = 1500, batch_size = train_ccaa, verbose = 1 )
+model.fit( train_x, train_y, epochs = 10000, batch_size = train_ccaa, verbose = 1 )
 
+### Guardamos el modelo en formato hdf5
 model.save( "model/covid_predictor.h5" )
 
+### Predecimos los próximos 30 días de castilla-La Mancha
 prediction = model.predict( clm_x, batch_size = len( target_cols ), verbose = 2 )
 
+### Y cambiamos el formato para que sea un vector de dimensión única
 prediction = np.resize( prediction.astype( int ), ( 30 ) )
 
-fig = plt.figure()
+### Generamos unos gráficos para validación visual
 plt.plot( prediction, label = "Predicción" )
-plt.plot(  clm_data.iloc[ 30 : 60, : ][ target_cols ].to_numpy(), label = "Datos experimentales" )
+plt.plot( clm_data.iloc[ 30 : 60, : ][ target_cols ].to_numpy(), label = "Datos experimentales" )
+plt.title( "Predicción de Castilla-La Mancha" )
 plt.legend()
 plt.show()
+plt.clf()
+
+for ccaa in similar_ca:
+    exp_data    = ccaa_data[ ccaa ][ target_cols ].to_numpy()[ 30 : 60, : ]
+    exp_data    = np.resize( exp_data, ( 1, 1, 30 ) )
+    
+    pred        = model.predict( exp_data, batch_size = 1 )
+    pred        = np.resize( pred.astype( int ), ( 30 ) )
+    
+    exp_data    = np.resize( exp_data, ( 30 ) )
+    
+    plt.plot( pred, label = "Predicción" )
+    plt.plot( exp_data, label = "Datos experimentales" )
+    plt.title( f"Predicción y datos empíricos de { ccaa }" )
+    plt.legend()
+    #plt.savefig( f"images/Prediction_{ ccaa }.pdf" )
+    plt.show()
+    plt.clf()

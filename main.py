7c4fc035df3c1f902e75620271de3ea5ad9fbdd4
Rober P. Romero <Roberto.Plaza@alu.uclm.es>
2020-05-30 10:26:14 +0200

    Final commit

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ distance_f  = lambda pair :len( pair[ 1 ].stepsTaken )
 ### Algunas constantes globales
 target_ccaa = "CM"                                                              # Nos centramos en Castilla-La Mancha
 target_cols = [ "CASOS" ]                                                       # Columna a tratar
-train_ccaa  = 8                                                                 # Número de comunidades autónomas las cuales vamos a entrenar
+train_ccaa  = 6                                                                 # Número de comunidades autónomas las cuales vamos a entrenar
 train_days  = 30                                                                # Número de días de entrenamiento para el dataset
-lookback    = 30                                                                # Memoria de la red neuranal
+lookback    = 25                                                                # Memoria de la red neuranal
 base_data   = pd.read_csv( "data/covid_data.csv" )                              # Dataframe con todo el contenido de la evolución del COVID-19                   
 ccaa_names  = base_data[ 'CCAA' ].unique()                                      # Nombres de las comunidades autónomas
 num_cols    = [ 'CASOS', 'Hospitalizados', 'UCI', 'Fallecidos', 'Recuperados' ] # Columnas con valores numéricos
@@ -47,7 +47,7 @@ similar_ca  = [ pair[ 0 ] for pair in sorted( distancias.items(), key = distance
 ### Preinicializamos los arrays de train y test
 train_x     = np.ones( ( train_ccaa, train_days, len( target_cols ) ) )         # Datos de entreno X
 train_y     = np.ones( ( train_ccaa, train_days, len( target_cols ) ) )         # Datos de entreno Y
-clm_x       = clm_data.iloc[ : train_days,  : ][ target_cols ].to_numpy()
+clm_x       = clm_data.iloc[ lookback : lookback + train_days,  : ][ target_cols ].to_numpy()
 
 ### Creamos los conjuntos de train y test
 for i in range( train_ccaa ):
@@ -63,13 +63,12 @@ clm_x       = np.reshape( clm_x, ( 1, 1, clm_x.shape[ 0 ] ) )
 ### Creamos el modelo de predicción con keras
 model       = Sequential( )
 model.add( LSTM( train_days, return_sequences = True, input_shape = ( len( target_cols ), train_days ) ) )
-
 model.add( LSTM( train_days * 21, return_sequences = True ) )
-model.add( Dense( 30 ) )
+model.add( Dense( train_days ) )
 
 ### Entrenamos el modelo
 model.compile( loss='mean_squared_error', optimizer='adam' )
-model.fit( train_x, train_y, epochs = 10000, batch_size = train_ccaa, verbose = 1 )
+model.fit( train_x, train_y, epochs = 15000, batch_size = train_ccaa, verbose = 1 )
 
 ### Guardamos el modelo en formato hdf5
 model.save( "model/covid_predictor.h5" )
@@ -78,11 +77,11 @@ model.save( "model/covid_predictor.h5" )
 prediction = model.predict( clm_x, batch_size = len( target_cols ), verbose = 2 )
 
 ### Y cambiamos el formato para que sea un vector de dimensión única
-prediction = np.resize( prediction.astype( int ), ( 30 ) )
+prediction = np.resize( prediction.astype( int ), ( prediction.shape[ 2 ] ) )
 
 ### Generamos unos gráficos para validación visual
 plt.plot( prediction, label = "Predicción" )
-plt.plot( clm_data.iloc[ 30 : 60, : ][ target_cols ].to_numpy(), label = "Datos experimentales" )
+plt.plot( clm_data.iloc[ lookback : train_days + lookback, : ][ target_cols ].to_numpy(), label = "Datos experimentales" )
 plt.title( "Predicción de Castilla-La Mancha" )
 plt.legend()
 plt.savefig( "images/Prediction_CLM.pdf" )
@@ -90,13 +89,13 @@ plt.show()
 plt.clf()
 
 for ccaa in ccaa_data:
-    exp_data    = ccaa_data[ ccaa ][ target_cols ].to_numpy()[ 30 : 60, : ]
-    exp_data    = np.resize( exp_data, ( 1, 1, 30 ) )
+    exp_data    = ccaa_data[ ccaa ][ target_cols ].to_numpy()[ lookback : train_days + lookback, : ]
+    exp_data    = np.resize( exp_data, ( 1, 1, train_days ) )
     
     pred        = model.predict( exp_data, batch_size = 1 )
-    pred        = np.resize( pred.astype( int ), ( 30 ) )
+    pred        = np.resize( pred.astype( int ), ( train_days ) )
     
-    exp_data    = np.resize( exp_data, ( 30 ) )
+    exp_data    = np.resize( exp_data, ( train_days ) )
     
     plt.plot( pred, label = "Predicción" )
     plt.plot( exp_data, label = "Datos experimentales" )
